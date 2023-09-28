652e736bb535f2397bf585f90e2a0c2b769fa633
Ganesh-BSSR <145440864+Ganesh-BSSR@users.noreply.github.com>
2023-09-28 16:29:27 +0530

    Added Content

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+const Content = () => <div>Enjoy Github actions by doing every bit of it</div>
+
 function App() {
   return (
     <div>
       <h2>Hello! Welcome to Ganesh BSSR's Repositories</h2>
+      <Content />
     </div>
   )
 }
